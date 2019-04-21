dd2f256b071782309f0b4afc13e94bc91397a930
Xplor8r <mpieschel39@gmail.com>
2019-04-21 07:59:11 -0700

    update app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,66 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Spinner from './Spinner'
+import Images from './Images'
+import Buttons from './Buttons'
+import { API_URL } from './config'
+import './App.css'
 
-class App extends Component {
+export default class App extends Component {
+  
+  state = {
+    uploading: false,
+    images: []
+  }
+
+  onChange = e => {
+    const files = Array.from(e.target.files)
+    this.setState({ uploading: true })
+
+    const formData = new FormData()
+
+    files.forEach((file, i) => {
+      formData.append(i, file)
+    })
+
+    fetch(`${API_URL}/image-upload`, {
+      method: 'POST',
+      body: formData
+    })
+    .then(res => res.json())
+    .then(images => {
+      this.setState({ 
+        uploading: false,
+        images
+      })
+    })
+  }
+
+  removeImage = id => {
+    this.setState({
+      images: this.state.images.filter(image => image.public_id !== id)
+    })
+  }
+  
   render() {
+    const { uploading, images } = this.state
+
+    const content = () => {
+      switch(true) {
+        case uploading:
+          return <Spinner />
+        case images.length > 0:
+          return <Images images={images} removeImage={this.removeImage} />
+        default:
+          return <Buttons onChange={this.onChange} />
+      }
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className='buttons'>
+          {content()}
+        </div>
       </div>
-    );
+    )
   }
 }
-
-export default App;
