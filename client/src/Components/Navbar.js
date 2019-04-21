b4c281ed8f614950741ff78118ea3169e6459fdb
Xplor8r <mpieschel39@gmail.com>
2019-04-21 11:49:30 -0700

    move images update navbar

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  Media,
   Collapse,
   Navbar,
   NavbarToggler,
@@ -11,6 +12,7 @@ import {
   DropdownToggle,
   DropdownMenu,
   DropdownItem } from 'reactstrap';
+import PuppyIcon from '../images/puppyg.jpg'
 
 export default class Example extends React.Component {
   constructor(props) {
@@ -29,31 +31,27 @@ export default class Example extends React.Component {
   render() {
     return (
       <div>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <Navbar color="dark" expand="md">
+          <NavbarBrand href="/"><Media src={PuppyIcon} style={{ height: 50}}/></NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
               <NavItem>
-                <NavLink href="/components/">Components</NavLink>
+                <NavLink className="text-warning" href="/Puppypics">View Puppy Pics</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                <NavLink className="text-warning" href="http://www.laanimalservices.com/">LA Animal Services</NavLink>
               </NavItem>
               <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
+                <DropdownToggle className="text-warning"  nav caret>
+                  Lost/Found
                 </DropdownToggle>
                 <DropdownMenu right>
                   <DropdownItem>
-                    Option 1
+                    I lost my puppy
                   </DropdownItem>
                   <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
+                    I found a lost puppy
                   </DropdownItem>
                 </DropdownMenu>
               </UncontrolledDropdown>
