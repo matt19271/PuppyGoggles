35c84284655c215ccf959bf3412385a4c411a82a
Xplor8r <mpieschel39@gmail.com>
2019-04-22 19:25:13 -0700

    fix typo

--- client/src/Components/HomePage.js
+++ client/src/Components/HomePage.js
@@ -9,7 +9,7 @@ const Home = () => {
         <Media style={{ width: "100%" }} src={PuppyLogo}></Media>
         <h1 className="display-3">Welcome to PuppyGoggles!</h1>
         <hr className="my-2" />
-        <p>Wheather you have lost your puppy or found a lost puppy we can help you connect the dogs!</p>
+        <p>Whether you have lost your puppy or found a lost puppy we can help you connect the dogs!</p>
         <p className="lead">
           <Button color="primary">Learn More</Button>
         </p>
